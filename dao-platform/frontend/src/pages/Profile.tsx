7b6fae5eb7f26b809f327024ac412a141d22151b
Andy <andy@transformai.app>
2025-09-09 15:35:14 -0400

    Updated

--- dao-platform/frontend/src/pages/Profile.tsx
+++
@@ -1,8 +0,0 @@
-export default function Profile() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-8">Profile</h1>
-      <p className="text-gray-600 dark:text-gray-300">User profile will be displayed here</p>
-    </div>
-  );
-}
